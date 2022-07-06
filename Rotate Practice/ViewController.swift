d78b3c03caf66635ff5d732814f52c2a38bfc3a1
paninology <paninology@gmail.com>
2022-07-06 09:12:21 +0900

    Initial Commit

---
+++ Rotate Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rotate Practice
+//
+//  Created by yongseok lee on 2022/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
